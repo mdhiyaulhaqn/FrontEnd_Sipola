13fc44f9d196d4b8b6c983514d649101f95d9081
yasminamalia <51574689+yasminamalia@users.noreply.github.com>
2020-04-11 23:33:55 +0700

    Uc08/mengelola invoice (#27)
    * feat: add invoice page
    
    * feat: update invoice page
    
    * view: invoice page
    
    * fixed code
    
    * fixed detail invoice
    
    * feat: form page

---
+++ src/pages/Finance/DetailSalesOrderForInvoice.vue
@@ -0,0 +1,244 @@
+<template>
+    <div class="row">
+        <div class = "col-12">
+            <div class="judul">
+                <strong>
+                    Detail Sales Order
+                </strong>
+            </div> 
+            
+            <card>
+                <b-row>
+                    <div class = "col-8 nama-perusahaan">{{sales_order.company.nama}}</div>
+                    <div class = "col-4">Created by : {{sales_order.createdBy}} <br>Created At : {{ sales_order.createdAt.split("T")[0].split("-").reverse().join('-') }}</div>
+                </b-row>
+                <b-row>
+                    <div class = "col-3">Sales Order No</div>
+                    <div class = "col-6">: {{sales_order.noSalesOrder}}</div>
+                </b-row>
+                <b-row>
+                    <div class = "col-3">Sales Order Date</div>
+                    <div class = "col-6">: {{ sales_order.date.split("T")[0].split("-").reverse().join('-') }}</div>
+                </b-row>
+                  <b-row>
+                    <div class = "col-3">Purchase Order No</div>
+                    <div class = "col-6">: {{ sales_order.poNumber }}</div>
+                </b-row>
+                  <b-row>
+                    <div class = "col-3">Purchase Order Date</div>
+                    <div class = "col-6">: {{ sales_order.poDate.split("T")[0].split("-").reverse().join('-') }}</div>
+                </b-row>
+                <b-row>
+                    <div class = "col-3">Address</div>
+                    <div class = "col-6">: {{sales_order.company.alamat}}</div>
+                </b-row>
+                <br>
+                
+                <b-row>
+                    <b-col >
+                        <div class="tabel-service">
+                            Service List
+                            <div slot="raw-content" class="table-responsive" style="font-size:12px">
+                                <b-table 
+                                :items="sales_order.serviceOrder" 
+                                :fields="fields">
+                                 <template v-slot:cell(no)="row">
+                                    {{sales_order.service.indexOf(row.item) + 1}}
+                                </template>
+                                </b-table>
+                                
+                            </div>
+                        </div>
+                    </b-col>
+                
+                </b-row>
+
+                <b-row>
+                    <div class = "col-12"><br>Terms and Condition</div>
+                    <div class = "col-12">{{sales_order.termsCondition}}
+                    </div>
+                </b-row>
+
+                <b-row>
+                   
+                    <div class="col">
+                        <br>
+                         <router-link :to="{name: 'invoice-add', params: {id: sales_order.id}}">
+                            <button id ="generate_button" class="btn btn-primary">
+                                Generate Invoice
+                            </button>
+                         </router-link>
+                    </div>
+                </b-row>
+
+            </card>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            sales_order : '',
+            successModal : false,
+            fields: [
+                {key: 'id', label: 'No', sortable: true},
+                {key: 'deskripsi', label: 'Description', sortable: true},
+                {key: 'uom', label: 'UOM', sortable: true},
+                {key: 'quantity', label: 'Quantity', sortable: true},
+                {key: 'pricePerUnit', label: 'Unit Price(IDR)', sortable: true},
+                {key: 'total_price', label:  'Total_Price(IDR)', sortable: true},
+            ]
+        };
+    },
+    beforeMount(){
+        this.getDetail();
+
+    },
+    methods:{
+        getDetail: function(){    
+            axios.get('http://localhost:8080/api/sales-order/' +this.$route.params.id)
+            .then(res => {this.sales_order = res.data})
+            .catch(err => this.sales_order = err.data);
+        },
+    },
+};
+</script>
+
+<style scoped>
+body {
+    font-family: 'Muli', sans-serif;
+    background: #fafafa;
+}
+
+.judul{
+    text-align: center;
+    color: black;
+    font-size:20px;
+    margin-bottom: 20px;
+}
+.nama-perusahaan{
+    color: black;
+    font-size:20px;
+    margin-bottom: 20px;
+}
+.tabel-service{
+    font-size:15px;
+}
+#download_button{
+    font-size: 10px;
+    float:right;
+    background-color: green;
+    color:white;
+    border-color: white;
+}
+
+#modal-download{
+    color:black;
+}
+
+.ti-download{
+    margin-left:10px;
+    margin-right: 10px;
+}
+
+.fa-warning{
+    margin-left:10px;
+    margin-right: 10px;
+}
+
+.modal-header{
+    background-color: #FF3E1D;
+}
+
+.button_back{
+    background-color: #FF3E1D;
+    color:white;
+    border-color: white;
+    float:right;
+    margin-top: 40px;
+}
+
+.button_oke{
+    background-color:white;
+    color:#FF3E1D;
+    border-color: #FF3E1D;
+    float:right;
+    margin-top: 40px;
+}
+
+.tombol_okay{
+    float:right;
+}
+
+#manage-button{
+    margin-left: auto;
+    margin-right: auto;
+}
+
+#delete_button{
+    font-size: 10px;
+    background-color: #ff3e1d;
+    color:white;
+    border-color: white;
+}
+#edit_button{
+    font-size: 10px;
+    background-color: #109CF1;
+    color:white;
+    border-color: white;
+}
+.button-group{
+    text-align: center;
+}
+
+button{
+    border-radius: 8px;
+}
+
+#delete_button{
+    font-size: 10px;
+    width: 56;
+    background-color: #ff3e1d;
+    color:white;
+    border-color: white;
+}
+#edit_button{
+    font-size: 10px;
+    width: 130px;
+    background-color: #109CF1;
+    color:white;
+    border-color: white;
+}
+
+.ti-trash{
+    font-size: 50px;
+    text-align: center;
+    color:#ff3e1d;
+}
+
+.button-confirm-group{
+    text-align: right;
+}
+
+#confirm_delete_button{
+    font-size: 10px;
+    width: 130px;
+    border-color: #ff3e1d;
+    border-width: 1px;
+    margin-right: 10px;
+}
+
+#cancel_delete_button{
+    font-size: 10px;
+    background-color: #ff3e1d;
+    color:white;
+    border-color: white;
+    border-width: 1px;
+}
+</style>
---
+++ src/pages/Finance/SalesOrderForInvoice.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="row">
+      <div class="col-12">
+        <card>
+          <b-row>
+           <router-link :to="{name: 'add-sales-order'}">
+            <button id ="add_quotation_button" class="btn btn-primary">
+              Add Sales Order
+              <span class="ti-plus"></span>
+            </button>
+          </router-link>
+          </b-row>
+                 
+          <div slot="raw-content" class="table-responsive">     
+            <b-table 
+                responsive
+                :items="items"
+                :fields="fields"
+                :per-page="akuntable.perPage" 
+                :current-page="akuntable.currentPage"
+                :sort-by.sync="akuntable.sortBy"
+                :sort-desc.sync="akuntable.sortDesc">
+                
+                <template v-slot:cell(id)="row">
+                   {{items.indexOf(row.item) + 1}}
+                </template>
+
+                <template v-slot:cell(date)="row">
+                    {{row.item.date.split("T")[0].split("-").reverse().join('-') }}
+                </template>
+<!-- 
+                 <template v-slot:cell(total_price) = "row">
+                    {{row.item.service[1].harga}}
+                </template> -->
+
+                <template v-slot:cell(Action)="row">
+                    <router-link :to="{name: 'detail-sales-order-for-invoice', params: {id:row.item.id}}">
+                        <b-button id ="add_quotation_button">
+                          view
+                        </b-button>
+                    </router-link>
+                </template>
+            </b-table>
+
+            <b-pagination class="pagination"
+              v-model="akuntable.currentPage"
+              :total-rows="sales_orders.length"
+              :per-page="akuntable.perPage"
+              aria-controls="myTable"/>
+          </div>
+        </card>
+      </div>
+
+
+  </div>
+</template>
+<script>
+import axios from 'axios';
+import DataTable from 'v-data-table'
+
+export default {
+
+  data() {
+    return {
+      akuntable : { 
+          currentPage : 1,
+          perPage : 5,
+          sortDesc : false,
+      },
+      
+      fields: [
+          {key: 'id', label: 'Id', sortable: true},
+          {key: 'noSalesOrder', label: 'Sales Order No', sortable: true},
+          {key: 'poNumber', label: 'Purchase Order No', sortable: true},
+          {key: 'company.nama', label: 'Company Nama', sortable:true},
+          {key: 'date', label: 'Date', sortable:true},
+          'Action'
+      ],
+      sales_orders :[],
+      keyword :'',
+    }
+  },
+   computed: {
+      items() {
+          return this.sales_orders;
+      },
+  },
+
+
+  beforeMount(){
+      this.getAllSalesOrder();
+      this.test();
+  },
+  methods:{
+      getAllSalesOrder: function(){
+          axios.get('http://localhost:8080/api/sales-order/all')
+          .then(result => this.sales_orders = result.data.result);
+      },
+      clear(){
+          this.keyword = '';
+      },
+  }
+};
+</script>
+<style>
+#add_quotation_button{
+  background-color: #109CF1;
+  color:white;
+  border-color: transparent;
+  font-size: 10px;
+  margin-bottom: 4px;
+  margin-left:20px;
+  box-shadow: 0px 0px 15px rgba(16, 156, 241, 0.2);
+}
+.judul{
+    text-align: center;
+    color: black;
+    margin: 5px 0 24px 0;
+}
+.pagination{
+  margin-left:20px;
+}
+</style>
