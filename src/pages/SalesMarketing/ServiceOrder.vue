b6d16ae7208f6356c53dc91d3e221375a7228e09
yasminamalia <51574689+yasminamalia@users.noreply.github.com>
2020-05-19 22:33:38 +0700

    Uc13/mengelola dashboard (#113)
    * dashboard is on her way
    
    * dashboard is building itself
    
    * commit
    
    * dashboard minus periode
    
    * ketinggalan
    
    * wanna pull
    
    * add condition for sales order
    
    * penerapan login
    
    * solve error
    
    * benerin beberapa hal
    
    * dashboard aneh

--- src/pages/SalesMarketing/ServiceOrder.vue
+++ src/pages/SalesMarketing/ServiceOrder.vue
@@ -1,74 +1,133 @@
 <template>
+    <div>
+        <!-- FOR SALES & MARKETING -->
+        <b-row class="service_order_row" v-if="currentUser().roles.includes('ROLE_SALES_MARKETING')">
+            <span class = "col-md-4 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
+                <label class="label">Description</label>
+            </span>
+            <div class = "col-md-4 col-sm-8 col-xs-8 col-7">
+                <b-form-input
+                    id="deskripsi"
+                    v-model="service_order.deskripsi"
+                    type="text"
+                    required
+                    placeholder="Description"
+                    >
+                </b-form-input>
+            </div>
 
-    <b-row class="service_order_row">
-        <span class = "col-md-6 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
-            <label class="label">Description</label>
-        </span>
-        <div class = "col-md-4 col-sm-8 col-xs-8 col-7">
-            <b-form-input
-                id="deskripsi"
-                v-model="service_order.deskripsi"
-                type="text"
-                required
-                placeholder="Description"
-                >
-            </b-form-input>
-        </div>
+            <span class = "col-md-2 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
+                <label class="label">Quantity</label>
+            </span>
+            <div class= "col-md-2 col-sm-8 col-xs-8 col-7">
+                <b-form-input
+                    id="quantity"
+                    v-model="service_order.quantity"
+                    type="number"
+                    required
+                    placeholder="0"
+                    >
+                </b-form-input>
+            </div>
 
-         <span class = "col-md-2 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
-            <label class="label">Quantity</label>
-        </span>
+            <span class = "col-md-2 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
+                <label class="label">UOM</label>
+            </span>
+            <div class= "col-md-2 col-sm-8 col-xs-8 col-7">
+                <b-form-input
+                    id="uom"
+                    v-model="service_order.uom"
+                    type="text"
+                    required
+                    placeholder="UOM"
+                    >
+                </b-form-input>
+            </div>
 
-         <div class= "col-md-2 col-sm-8 col-xs-8 col-7">
-            <b-form-input
-                id="quantity"
-                v-model="service_order.quantity"
-                type="number"
-                required
-                placeholder="0"
-                >
-            </b-form-input>
-        </div>
+            <span class = "col-md-3 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
+                <label class="label">Unit Price (IDR)</label>
+            </span>
+            <div class= "col-md-3 col-sm-8 col-xs-8 col-7">
+                <b-form-input
+                    id="pricePerUnit"
+                    v-model="service_order.pricePerUnit"
+                    type="number"
+                    required
+                    placeholder="0"
+                    >
+                </b-form-input>
+            </div>
 
-         <span class = "col-md-2 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
-            <label class="label">UOM</label>
-        </span>
+            <span class = "col-md-1 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none">
+            </span>
+            <div class = "col-md-1   col-sm-8 col-xs-8 col-7" >
+                <b-button @click="$emit('del-service-order', service_order.id_service_order)" variant="danger"><i class="fas fa-trash-alt"></i></b-button>
+            </div>
+        </b-row>
 
-         <div class= "col-md-2 col-sm-8 col-xs-8 col-7">
-            <b-form-input
-                id="uom"
-                v-model="service_order.uom"
-                type="text"
-                required
-                placeholder="UOM"
-                >
-            </b-form-input>
-        </div>
+        <!-- FOR FINANCE -->
+        <b-row class="service_order_row" v-if="currentUser().roles.includes('ROLE_FINANCE')">
+            <span class = "col-md-5 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
+                <label class="label">Description</label>
+            </span>
+            <div class = "col-md-5 col-sm-8 col-xs-8 col-7">
+                <b-form-input
+                    id="deskripsi"
+                    v-model="service_order.deskripsi"
+                    type="text"
+                    required
+                    placeholder="Description"
+                    disabled
+                    >
+                </b-form-input>
+            </div>
 
-         <span class = "col-md-2 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
-            <label class="label">Unit Price (IDR)</label>
-        </span>
+            <span class = "col-md-2 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
+                <label class="label">Quantity</label>
+            </span>
+            <div class= "col-md-2 col-sm-8 col-xs-8 col-7">
+                <b-form-input
+                    id="quantity"
+                    v-model="service_order.quantity"
+                    type="number"
+                    required
+                    placeholder="0"
+                    disabled
+                    >
+                </b-form-input>
+            </div>
 
-        <div class= "col-md-3 col-sm-8 col-xs-8 col-7">
-            <b-form-input
-                id="pricePerUnit"
-                v-model="service_order.pricePerUnit"
-                type="number"
-                required
-                placeholder="0"
-                >
-            </b-form-input>
-        </div>
+            <span class = "col-md-2 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
+                <label class="label">UOM</label>
+            </span>
+            <div class= "col-md-2 col-sm-8 col-xs-8 col-7">
+                <b-form-input
+                    id="uom"
+                    v-model="service_order.uom"
+                    type="text"
+                    required
+                    placeholder="UOM"
+                    disabled
+                    >
+                </b-form-input>
+            </div>
 
-
-        <span class = "col-md-1 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none" v-if="currentUser().roles.includes('ROLE_SALES_MARKETING')">
-        </span>
-        <div class = "col-md-1   col-sm-8 col-xs-8 col-7" v-if="currentUser().roles.includes('ROLE_SALES_MARKETING')">
-            <b-button @click="$emit('del-service-order', service_order.id_service_order)" variant="danger"><i class="fas fa-trash-alt"></i></b-button>
-        </div>
-
-    </b-row>
-
+            <span class = "col-md-3 col-sm-4 col-xs-4 col-5 d-block d-xs-block d-sm-block d-md-none required">
+                <label class="label">Unit Price (IDR)</label>
+            </span>
+            <div class= "col-md-3 col-sm-8 col-xs-8 col-7">
+                <b-form-input
+                    id="pricePerUnit"
+                    v-model="service_order.pricePerUnit"
+                    type="number"
+                    required
+                    placeholder="0"
+                    disabled
+                    >
+                </b-form-input>
+            </div>
+        </b-row>
+    </div>
 </template>
 
 
@@ -90,7 +149,7 @@ export default {
 <style scoped>
 .service_order_row{
     margin-bottom: 10px;
-    margin-right: 0px;
+    margin-left: 0;
 }
 .label{
   font-weight: 600;
