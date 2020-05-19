4919f54071eebed11e61c934e8d537501ec53c55
Muhammad Dhiyaulhaq Nugraha <dhiyaulhaqnugraha@gmail.com>
2020-05-19 23:56:35 +0700

    fix: add margin loading

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -50,7 +50,7 @@
         </div>
         <div class="form-group">
           <button class="btn btn-primary btn-block" :disabled="loading">
-            <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+            <span v-show="loading" class="spinner-border spinner-border-sm" style="margin-right:10px;"></span>
             <span>Login</span>
           </button>
         </div>
