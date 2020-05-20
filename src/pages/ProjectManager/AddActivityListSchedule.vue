1cb66daa711ffdd3a9244da64105c3e12b8eaa2b
ringgicahyo <34148535+ringgicahyo@users.noreply.github.com>
2020-05-20 13:11:59 +0700

    Uc06/rev (#125)
    * feat: add Project Manager page for activity list schedule and create api
    
    * feat: add menu display in sidebar
    
    * template: edit design and layout for template
    
    * update functionality
    
    * resolves minor conflict
    
    * update detail page
    
    * fix delete feature and change server port
    
    * edit modal
    
    * finish update
    
    * fix minor conflicts in routes.js
    
    * add breadcrumbs on all activity list schedule pages
    
    * update design
    
    * edit minor icon
    
    * fix minor conflicts
    
    * fix minor conflicts
    
    * finish uc05 iteration 1, update minor button design changes
    
    * update design
    
    * edit regex pattern form
    
    * finish uc06
    
    * make responsive ui of components in all list page in daily activity schedule and activity list schedule
    
    * match new frontend and backend in list daftar tugas
    
    * fix table default order
    
    * customize table design
    
    * change retrievement of detail activity list schedule
    
    * beautify css design in all table pages
    
    * beautify css design in all table pages
    
    * update design
    
    * update design
    
    * change minor design quotation pages
    
    * change minor design sales order pages
    
    * update design activity list schedule pages
    
    * update design purchase order pages
    
    * update design purchase order pages
    
    * update design expense pages
    
    * update design reimbursement report pages
    
    * update design reimbursement request pages
    
    * update minor changes
    
    * fix minor errors in every pages
    
    * change some lines in detail activity list schedule page
    
    * fix bugs in modal header color, update ui design sales orer for invoice
    
    * change view button design in reimbursement report detail
    
    * update dashboard
    
    * update dashboard
    
    * update dashboard and sidebar
    
    * finish dashboard temporary
    
    * update dashboard
    
    * update axios url for deployment
    
    * update axios url for deployment
    
    * fix daily activity report and activity list schedule post error
    
    * change add backend ssl endpoint from http to https in some pages
    
    * fix navbar, sidebar
    
    * updated ui design, add icons
    
    * fix href link for top profile
    
    * fix sidebar link and auth header for signup
    
    * add import auth header for signup
    
    * fix ui design for invoice pages
    
    * update ui design sales order pages
    
    * fix icon sidebar link position
    
    * fix sidebar link position
    
    * fix sidebar link position, delete unneccessary files
    
    * update design and functionality add activity list schedule
    
    * update design for update activity list schedule page

--- src/pages/ProjectManager/AddActivityListSchedule.vue
+++ src/pages/ProjectManager/AddActivityListSchedule.vue
@@ -43,8 +43,8 @@
                   </b-form-input>
                 </b-form-group>
 
-                <div class="col-12 d-none d-md-block d-lg-block">
-                  <div class="row">
+                <div class="d-none d-md-block d-lg-block">
+                  <div class="row" style="margin: 0 -15px -12px -15px;">
                     <div class = "col-md-5 required">
                       <label class="label">Activity Name</label>
                     </div>
@@ -60,15 +60,15 @@
                   </div>
                 </div>
 
-                <b-row class="activities" v-bind:key="item.id_activity" v-for="item in activities">
+                <b-row class="activities" v-bind:key="item.id_activity" v-for="item in activities" >
                   <b-col>
                     <Activity v-bind:activity="item" v-on:del-activity="deleteRow" />
                   </b-col>
                 </b-row>
 
-                <b-row>
-                  <div class ="col-12 col-sm-5">
-                    <button class="btn btn-primary add-button" @click="addRow()" variant="outline-primary">Add More Activity <span><img src="@/assets/img/add-circle-blue-icon.png" alt="" width="25px"></span></button>
+                <b-row style="margin-top: 2px;">
+                  <div class ="col-12 col-md-4">
+                    <button class="btn btn-primary add-button" @click="addRow()">Add More Activity <span><img src="@/assets/img/add-circle-blue-icon.png" alt="" width="25px"></span></button>
                   </div>
                 </b-row>
 
@@ -221,15 +221,15 @@ export default {
 
 .add-button{
   width: 100%;
+  color:#109CF1;
+  border-color:#109CF1;
   background-color: white;
-  color : #109cf1;
-  border-color: #109cf1;
-  margin-bottom: 10px;
-  border-width: 1.5px;
-  line-height: 12px;
+  border-width: 2px;
+  line-height: 20px;
   text-align: center;
   font-size: 14px;
-  font-weight: 500;
+  margin-bottom: 10px;
+  border-radius: 5px;
 }
 .judul{
   text-align: center;
