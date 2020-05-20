66be4c5cc7d391d82af7881d93de92399ca5dd55
ringgicahyo <34148535+ringgicahyo@users.noreply.github.com>
2020-05-20 23:35:32 +0700

    Uc06/rev (#140)
    * feat: add Project Manager page for activity list schedule and create api
    
    * feat: add menu display in sidebar
    
    * template: edit design and layout for template
    
    * update functionality
    
    * resolves minor conflict
    
    * update detail page
    
    * fix delete feature and change server port
    
    * edit modal
    
    * finish update
    
    * fix minor conflicts in routes.js
    
    * add breadcrumbs on all activity list schedule pages
    
    * update design
    
    * edit minor icon
    
    * fix minor conflicts
    
    * fix minor conflicts
    
    * finish uc05 iteration 1, update minor button design changes
    
    * update design
    
    * edit regex pattern form
    
    * finish uc06
    
    * make responsive ui of components in all list page in daily activity schedule and activity list schedule
    
    * match new frontend and backend in list daftar tugas
    
    * fix table default order
    
    * customize table design
    
    * change retrievement of detail activity list schedule
    
    * beautify css design in all table pages
    
    * beautify css design in all table pages
    
    * update design
    
    * update design
    
    * change minor design quotation pages
    
    * change minor design sales order pages
    
    * update design activity list schedule pages
    
    * update design purchase order pages
    
    * update design purchase order pages
    
    * update design expense pages
    
    * update design reimbursement report pages
    
    * update design reimbursement request pages
    
    * update minor changes
    
    * fix minor errors in every pages
    
    * change some lines in detail activity list schedule page
    
    * fix bugs in modal header color, update ui design sales orer for invoice
    
    * change view button design in reimbursement report detail
    
    * update dashboard
    
    * update dashboard
    
    * update dashboard and sidebar
    
    * finish dashboard temporary
    
    * update dashboard
    
    * update axios url for deployment
    
    * update axios url for deployment
    
    * fix daily activity report and activity list schedule post error
    
    * change add backend ssl endpoint from http to https in some pages
    
    * fix navbar, sidebar
    
    * updated ui design, add icons
    
    * fix href link for top profile
    
    * fix sidebar link and auth header for signup
    
    * add import auth header for signup
    
    * fix ui design for invoice pages
    
    * update ui design sales order pages
    
    * fix icon sidebar link position
    
    * fix sidebar link position
    
    * fix sidebar link position, delete unneccessary files
    
    * update design and functionality add activity list schedule
    
    * update design for update activity list schedule page
    
    * update design for activity list schedule page, sales order, and quotation form
    
    * update design for delivery order form pages
    
    * update design for purchase order form pages
    
    * fix detail reimbursement finance error and expense design
    
    * edit minor modal message
    
    * edit minor modal message in activity list schedule pages
    
    * edit minor modal message in daily activity report pages
    
    * update design list user table
    
    * update user pages design
    
    * fix bugs in pagination logic
    
    * edit not found page and forbidden page
    
    * add routing homepage
    
    * edit login page design
    
    * fix homepage sidebar position and prevent row delete in activity list schedule form page
    
    * update design reimbursement report pages
    
    * update modal design in pengeluaran pages

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -8,9 +8,9 @@
               <img class="logo-sipola fluid" src="@/assets/img/sipola-icon.png" alt="">
             </div>
             <div class="col-md-8 col-md-offset-2">
-              <h1 class="title text-danger">Welcome to SIPOLA!</h1>
-              <h3 class="sub-title">Sistem Informasi Pengelolaan Penjualan</h3>
-              <h3 class="title">PT SixAB Tehnik Industri</h3>
+              <h1 class="title text-danger"><b>Welcome to SIPOLA!</b></h1>
+              <h3 class="sub-title"><b>Sistem Informasi Pengelolaan Penjualan</b></h3>
+              <h3 class="title"><b>PT SixAB Tehnik Industri</b></h3>
             </div>
           </div>
         </div>
